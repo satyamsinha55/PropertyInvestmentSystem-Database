dadd1ebc0f3bed9a6a43186a5efe56865b6db33a
Satyam Sinha <47027389+satyamsinha55@users.noreply.github.com>
2023-06-10 00:20:27 -0400

    Delete SQL for dropping DB directory

--- SQL for dropping DB/drop.sql
+++
@@ -1,7 +0,0 @@
-Drop Table Income CASCADE;
-Drop Table OwnerDetails CASCADE;
-Drop Table PropertyType CASCADE;
-Drop Table HeatingType CASCADE;
-Drop Table Address CASCADE;
-Drop Table StateDetails CASCADE;
-Drop Table House CASCADE;
